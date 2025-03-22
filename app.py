4bb0e7a4f3af74357fa91bb6dbc7881cae4e5bc6
ryancode123 <ttthekhoicr7@gmail.com>
2025-03-22 18:35:56 +0700

    Cập nhật app.py và index.html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -6,5 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# Route để phục vụ file tĩnh
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
